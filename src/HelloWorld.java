803d029edaace6464883a18924406f15ed432e5e
Bruno Silveira <dasilveira.bru@terra.com.br>
2022-01-25 10:42:50 -0300

    Primeiro commit, teste das config do projeto

---
+++ src/HelloWorld.java
@@ -0,0 +1,14 @@
+
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		Human person = new Human("Joel", 44);
+		person.sayName();
+		if (person.canDrink()) {
+			System.out.println(person.getName() + " can drink!");
+		}
+		else {
+			System.out.println(person.getName() + " is too young to drink!");
+		}
+	}
+}
