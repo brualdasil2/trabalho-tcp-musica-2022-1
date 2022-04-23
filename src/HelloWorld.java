727a871b0d49cfccba9aedf6e270de3619a90966
Leonardo Alles <leonardoralles@gmail.com>
2022-04-23 17:12:49 -0300

    Classe Reader

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -6,5 +6,15 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		Display display = new Display("Toca musica", 1280, 720);
 		ScreenManager sm = new ScreenManager(display);
+		Reader.setMusicString("aaa \n!.l;,?AAGCDFD ");
+		String r = "";
+		String read = "";
+		while(read != "END"){
+			read =Reader.read();
+			if (read != "END"){
+				r += read;
+			}
+		}
+		System.out.println(r);
 	}
 }
---
+++ src/Reader.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.util.*;
+
+public class Reader {
+    private static int currentPos = 0;
+    private static String musicString;
+    private static String lastCommandCode;
+
+    public static String read(){
+        String currentCommand;
+        try {
+            currentCommand = Character.toString(musicString.charAt(currentPos));
+        }
+        catch(Exception e) {
+            return "END";
+        }
+        currentPos ++;
+        lastCommandCode = currentCommand;
+        if( !isValidCommand(currentCommand) ){
+            return "ELSE";
+        }
+        return currentCommand;
+    }
+
+    // chamado na interface
+    public static void setMusicString(String newString){
+        musicString = newString;
+    }
+
+    public static void resetPos() { currentPos = 0; }
+    public static int GetCurrentPos(){
+        return currentPos;
+    }
+
+    private static Boolean isValidCommand(String command){
+        String[] validCommands = {" ","!","?",".",System.lineSeparator(),";",","};
+        for (String s : validCommands) {
+            if(command.charAt(0) == s.charAt(0)){
+                return true;
+            }
+        }
+        if(Character.isLetter(command.charAt(0)) || Character.isDigit(command.charAt(0))){
+            return true;
+        }
+        return false;
+    }
+}
+
+
