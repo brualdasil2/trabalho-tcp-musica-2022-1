9879bd6254fdd512981fb85e737d887e2259d41c
Leonardo <leo@pop-os.localdomain>
2022-04-26 10:51:23 -0300

    initial structure

---
+++ src/Commands.java
@@ -0,0 +1,32 @@
+public class Commands {
+    public void IncreaseVolume(){
+
+    }
+    public void DecreaseVolume(){
+
+    }
+    public void PlayNote(char note){
+
+    }
+    public void IncreaseOctave(){
+
+    }
+    public void DecreaseOctave(){
+
+    }
+    public void PlayRandomNote(){
+
+    }
+    public void ChangeInstrument(){
+
+    }
+    public void IncreaseBPM(){
+
+    }
+    public void DecreaseBPM(){
+
+    }
+    public void DoNothing(){
+
+    }
+}
