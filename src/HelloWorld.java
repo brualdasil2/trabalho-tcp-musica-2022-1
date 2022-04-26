e6c92be68700751652ab68c7721e9584f27d3253
Leonardo <leo@pop-os.localdomain>
2022-04-26 13:54:09 -0300

    Testing Command Class

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,11 +2,35 @@ import java.awt.Color;
 import java.awt.event.*;
 
 import javax.swing.*;
+
+import org.jfugue.pattern.Pattern;
+import org.jfugue.player.Player;
+
 public class HelloWorld {
 	public static void main(String[] args) {
-
-		Display display = new Display("Toca musica", 1280, 720);
-		ScreenManager sm = new ScreenManager(display);
+		Sound.Initialize();
+		Commands.PlayNote('C');
+		Commands.PlayNote('D');
+		Commands.PlayNote('E');
+		Commands.PlayNote('F');
+		Commands.PlayNote('G');
+		Commands.PlayNote('A');
+		Commands.PlayNote('B');
+		Commands.IncreaseOctave();
+		Commands.PlayNote('C');
+		Commands.PlayNote('D');
+		Commands.PlayNote('E');
+		Commands.PlayNote('F');
+		Commands.PlayNote('G');
+		Commands.PlayNote('A');
+		Commands.PlayNote('B');
+		Commands.DoNothing();
+		Commands.ChangeInstrument(114);
+		Commands.PlayRandomNote();
+		Commands.PlayRandomNote();
+		Commands.PlayRandomNote();
+		Commands.DoNothing();
+		Sound.playMusic();
 
 	}
 }
