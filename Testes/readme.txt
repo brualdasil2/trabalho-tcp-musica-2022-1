01792743a7048f41ad50e83c6fb7fce8c6fb145c
Bruno Silveira <dasilveira.bru@terra.com.br>
2022-05-02 23:33:34 -0300

    Tester criado com 8 casos teste

---
+++ Testes/readme.txt
@@ -0,0 +1,21 @@
+Configurações:
+- defaultVolume: 24
+- defaultBOM: 60
+- defaultOctave: 3
+
+Testes (copiar e colar ignorando o primeiro espaço)
+- Gerar arquivo musica.midi a partir do sistema
+- Renomear para TX.midi (X = número do teste)
+- Mover para a pasta "A Testar"
+- Compilar e rodar tester.c
+
+
+T1: ABCDEFG1AB?AB.ABCcDJEXFG
+T2: CDE
+CD!CD CD
+T3: CDOCDE;CDuCD
+T4: CDjkCDkkkkD
+T5: CDE()=>{😀}E
+T6: EF,hF
+T7: CD          CD
+T8: CD?.?...??CD
---
+++ Testes/tester.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+    FILE* testFile, *gabFile;
+
+    char testNumber[10] = "1";
+    int erro = 0;
+    while (!erro) {
+        char testFilePath[100] = "./A Testar/T";
+        char gabFilePath[100] = "./Gabarito/T";
+        strcat(testFilePath, testNumber);
+        strcat(testFilePath, ".midi");
+        strcat(gabFilePath, testNumber);
+        strcat(gabFilePath, ".midi");
+        testFile = fopen(testFilePath, "rb");
+        gabFile = fopen(gabFilePath, "rb");
+
+        char tb, gb;
+        int bytesRead = 0, equalBytes = 0;
+        do {
+            if (fread(&tb, sizeof(char), 1, testFile) != 1) {
+                erro = 1;
+            }
+            if (fread(&gb, sizeof(char), 1, gabFile) != 1) {
+                erro = 1;
+            };
+            if (!erro) {
+                bytesRead++;;
+                if (tb == gb) {
+                    equalBytes++;
+                }
+            }
+        } while (tb != EOF && gb != EOF);
+
+        printf("T%s: %.2f%%\n", testNumber, ((float) (equalBytes/bytesRead))*100);
+        testNumber[0]++;
+    }
+    return 0;
+}
