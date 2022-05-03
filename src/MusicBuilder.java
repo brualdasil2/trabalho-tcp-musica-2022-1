1f6df350c7d12fd2c6cb1e63173d17cff8080e5f
Bruno Silveira <dasilveira.bru@terra.com.br>
2022-05-02 21:31:25 -0300

    Criada classe MusicBuilder

---
+++ src/MusicBuilder.java
@@ -0,0 +1,64 @@
+
+public class MusicBuilder {
+	public static void buildMusic() {
+		Sound.Initialize();
+		Reader.resetPos();
+		String selectedCommand;
+		String previousCommand = "a";
+		boolean recording = true;
+		
+		while( recording ) {
+			
+			selectedCommand = Reader.read();
+			System.out.println("leu " + selectedCommand);
+					
+			if ("ABCDEFG".indexOf(selectedCommand) != -1) {
+				Commands.PlayNote(selectedCommand.charAt(0));
+			}
+			else if ("abcdefgjklmnpqrstvwxzJKLMNPQRSTVWXZ".indexOf(selectedCommand) != -1) {
+				if ("ABCDEFG".indexOf(previousCommand) != -1) {
+					Commands.PlayNote(previousCommand.charAt(0));
+				}
+				else {
+					Commands.DoNothing();
+				}
+			}
+			else if (" ".equals(selectedCommand)) {
+				Commands.IncreaseVolume();
+			}
+			else if ("!".equals(selectedCommand)) {
+				Commands.ChangeInstrument(114);//Agogo
+			}
+			else if ("OIU".indexOf(selectedCommand) != -1) {
+				Commands.ChangeInstrument(7);//Harpsichord
+			}
+			else if (Character.isDigit(selectedCommand.charAt(0))) {
+				Commands.ChangeInstrument(Sound.getCurrentInstrument() + Integer.parseInt(selectedCommand));
+			}
+			else if ("?.".indexOf(selectedCommand) != -1) {
+				Commands.IncreaseOctave();
+			}
+			else if ("\n".equals(selectedCommand)) {
+				Commands.ChangeInstrument(15);//Tubular Bells
+			}
+			else if (";".equals(selectedCommand)) {
+				Commands.ChangeInstrument(76);//Pan Flute
+			}
+			else if (",".equals(selectedCommand)) {
+				Commands.ChangeInstrument(20);//Church Organ
+			}
+			else if ("END".equals(selectedCommand)) {
+				recording = false;
+			}
+			else {
+				if ("ABCDEFG".indexOf(previousCommand) != -1) {
+					Commands.PlayNote(previousCommand.charAt(0));
+				}
+				else {
+					Commands.DoNothing();
+				}
+			}			
+			previousCommand = selectedCommand;	
+		}
+	}
+}
--- src/MusicPlayer.java
+++ src/MusicPlayer.java
@@ -43,67 +43,9 @@ public class MusicPlayer {
 		if (status != STOPPED) 
 			return;
 		
-		Sound.Initialize();
-		Reader.resetPos();
-		String selectedCommand;
-		String previousCommand = "a";
-		
 		status = PLAYING;
-		boolean building = true;
 		
-		while( building ) {
-			
-			selectedCommand = Reader.read();
-			
-			if ("ABCDEFG".indexOf(selectedCommand) != -1) {
-				Commands.PlayNote(selectedCommand.charAt(0));
-			}
-			else if ("abcdefgjklmnpqrstvwxzJKLMNPQRSTVWXZ".indexOf(selectedCommand) != -1) {
-				if ("ABCDEFG".indexOf(previousCommand) != -1) {
-					Commands.PlayNote(previousCommand.charAt(0));
-				}
-				else {
-					Commands.DoNothing();
-				}
-			}
-			else if (" ".equals(selectedCommand)) {
-				Commands.IncreaseVolume();
-			}
-			else if ("!".equals(selectedCommand)) {
-				Commands.ChangeInstrument(114);//Agogo
-			}
-			else if ("OIU".indexOf(selectedCommand) != -1) {
-				Commands.ChangeInstrument(7);//Harpsichord
-			}
-			else if (Character.isDigit(selectedCommand.charAt(0))) {
-				Commands.ChangeInstrument(Sound.getCurrentInstrument() + Integer.parseInt(selectedCommand));
-			}
-			else if ("?.".indexOf(selectedCommand) != -1) {
-				Commands.IncreaseOctave();
-			}
-			else if ("\n".equals(selectedCommand)) {
-				Commands.ChangeInstrument(15);//Tubular Bells
-			}
-			else if (";".equals(selectedCommand)) {
-				Commands.ChangeInstrument(76);//Pan Flute
-			}
-			else if (",".equals(selectedCommand)) {
-				Commands.ChangeInstrument(20);//Church Organ
-			}
-			else if ("END".equals(selectedCommand)) {
-				building = false;
-			}
-			else {
-				if ("ABCDEFG".indexOf(previousCommand) != -1) {
-					Commands.PlayNote(previousCommand.charAt(0));
-				}
-				else {
-					Commands.DoNothing();
-				}
-			}			
-			
-			previousCommand = selectedCommand;
-		}
+		MusicBuilder.buildMusic();
 		Sound.playMusic();
 		Stop();
 	}
