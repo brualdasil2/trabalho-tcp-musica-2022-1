457393f911209b34a5beac8d8005d8fa4da238c8
Bruno Silveira <dasilveira.bru@terra.com.br>
2022-05-03 21:51:23 -0300

    Instrument mas n passa no teste

---
+++ src/Instrument.java
@@ -0,0 +1,24 @@
+public class Instrument {
+	private int midiCode;
+	private static final int MIN_CODE = 0;
+	private static final int MAX_CODE = 128;
+
+	public Instrument() {
+		midiCode = 0;
+	}
+	public Instrument(int midiCode) {
+		setMidiCode(midiCode);
+	}
+
+	public int getMidiCode() {
+		return midiCode;
+	}
+	public void setMidiCode(int newCode) {
+		if (newCode > Instrument.MAX_CODE) {
+			midiCode = Instrument.MIN_CODE;
+		}
+		else {
+			midiCode = newCode;
+		}
+	}
+}
